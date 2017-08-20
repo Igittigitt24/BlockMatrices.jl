ce3f289e585b87b906f74a6c87756f661ff1a873
Mattias Fält <faldt.mattias@gmail.com>
2017-08-20 23:19:51 +0200

    Initial working version

--- README.md
+++ README.md
@@ -5,3 +5,50 @@
 [![Coverage Status](https://coveralls.io/repos/mfalt/BlockMatrices.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/mfalt/BlockMatrices.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/mfalt/BlockMatrices.jl/coverage.svg?branch=master)](http://codecov.io/github/mfalt/BlockMatrices.jl?branch=master)
+
+Implements Block Matrices as an efficient alternative to concatenating matrices of different types.
+
+The package supplies the sized `ZeroMatrix` and `IdentityMatrix`.
+
+Supported methods: `A_mul_B!, At_mul_B!, Ac_mul_B!`, i.e. also `A*b, A'b, A.'b'`.
+
+### Examples
+
+Time and allocation comparison for large and sparse blocks:
+```julia
+A = randn(2000,2000);
+B = ZeroMatrix{Float64}(2000,20000);
+C = IdentityMatrix{Float64}(2.0,2000);
+D = sprandn(2000,20000,0.001);
+
+#Create [A B; C D] BlockMatrix
+M1 = BlockMatrix(((A,B),(C,D)));          # 17.312 μs  (140 allocations: 176.73 KiB)
+M2 = [A spzeros(2000,20000); 2I D];       # 66.354 ms  (109 allocations: 215.98 MiB)
+M3 = full([A spzeros(2000,20000); 2I D]); # 236.966 ms (111 allocations: 887.36 MiB)
+
+b = randn(size(M,2));
+y = randn(size(M,1));
+
+A_mul_B!(y, M1, b); #  1.692 ms (9 allocations: 432 bytes)
+A_mul_B!(y, M2, b); #  4.049 ms (0 allocations: 0 bytes)
+A_mul_B!(y, M3, b); # 32.707 ms (0 allocations: 0 bytes)
+```
+
+For smaller blocks:
+```julia
+A = randn(20,20);
+B = ZeroMatrix{Float64}(20,20);
+C = IdentityMatrix{Float64}(2.0,20);
+D = sprandn(20,20,0.1);
+
+M1 = BlockMatrix(((A,B),(C,D)));  # 17.069 μs (113 allocations: 4.81 KiB)
+M2 = [A spzeros(20,20); 2I D];    # 17.565 μs (81 allocations: 31.30 KiB)
+M3 = full([A zeros(20,20); 2I D]) # 19.735 μs (64 allocations: 45.48 KiB)
+
+b = randn(size(M,2));
+y = randn(size(M,1));
+
+A_mul_B!(y, M1, b); # 394.328 ns (9 allocations: 432 bytes)
+A_mul_B!(y, M2, b); # 464.604 ns (0 allocations: 0 bytes)
+A_mul_B!(y, M3, b); # 233.207 ns (0 allocations: 0 bytes)
+```
--- src/BlockMatrices.jl
+++ src/BlockMatrices.jl
@@ -1,5 +1,148 @@
 module BlockMatrices
 
-# package code goes here
+export ZeroMatrix, IdentityMatrix, BlockMatrix
+
+abstract type AbstractBlockMatrix{T} <: AbstractArray{T,2} end
+
+Base.size(A::AbstractBlockMatrix) = (A.m, A.n)
+
+struct ZeroMatrix{T} <: AbstractMatrix{T}
+    m::Int
+    n::Int
+end
+Base.size(A::ZeroMatrix) = (A.m, A.n)
+
+function Base.A_mul_B!{T}(Y::AbstractVector{T}, A::ZeroMatrix{T}, B::AbstractVector{T})
+    @assert size(A,2) == size(B,1)
+    @assert size(A,1) == size(Y,1)
+    fill!(Y, zero(T))
+end
+function Base.At_mul_B!{T}(Y::AbstractVector{T}, A::ZeroMatrix{T}, B::AbstractVector{T})
+    @assert size(A,1) == size(B,1)
+    @assert size(A,2) == size(Y,1)
+    fill!(Y, zero(T))
+end
+Base.Ac_mul_B!{T}(Y::AbstractVector{T}, A::ZeroMatrix{T}, B::AbstractVector{T}) = At_mul_B!(Y,A,B)
+
+struct IdentityMatrix{T} <: AbstractMatrix{T}
+    m::Int
+    λ::T
+    IdentityMatrix{T}(λ::T,s) where {T} = new(s,λ)
+end
+Base.size(A::IdentityMatrix) = (A.m, A.m)
+
+function Base.A_mul_B!{T}(Y::AbstractVector{T}, A::IdentityMatrix{T}, B::AbstractVector{T})
+    @assert size(A,2) == size(B,1)
+    @assert size(A,1) == size(Y,1)
+    copy!(Y, B)
+    scale!(Y, A.λ)
+    return Y
+end
+Base.At_mul_B!{T}(Y::AbstractVector{T}, A::IdentityMatrix{T}, B::AbstractVector{T}) = A_mul_B!(Y, A, B)
+Base.Ac_mul_B!{T}(Y::AbstractVector{T}, A::IdentityMatrix{T}, B::AbstractVector{T}) = A_mul_B!(Y, A, B)
+
+"""
+    M = BlockMatrix{T}(Mtuple) <: AbstractBlockMatrix{T} <: AbstractArray{T,2}
+
+BlockMatrix of form `[A11 A12 ...; A21 A22 ...; ...]` where `Mtuple = ((A11,A12,...),(A21,A22,...),...)`
+and each `Aij<:AbstractArray{T}``
+"""
+struct BlockMatrix{T, MT<:NTuple{<:Any,NTuple{<:Any,AbstractArray}}} <: AbstractBlockMatrix{T}
+    m::Int
+    n::Int
+    M::MT
+    mranges::Array{UnitRange{Int},1}
+    nranges::Array{UnitRange{Int},1}
+    tmp::Array{T,1}         #Size: max(m,n)
+    function BlockMatrix{T,MT}(M::MT) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}}
+        hs = Array{Int,1}(mi)
+        ws = Array{Int,1}(ni)
+        for i = 1:mi                #All rows have equal hight
+            h = size(M[i][1],1)
+            for j = 1:ni
+                @assert size(M[i][j],1) == h
+            end
+            hs[i] = h
+        end
+        for j = 1:ni                #All columns have equal width
+            w = size(M[1][j],2)
+            for i = 1:mi
+                @assert size(M[i][j],2) == w
+            end
+            ws[j] = w
+        end
+        cumh = cumsum([0;hs])
+        cumw = cumsum([0;ws])
+        mranges = [(cumh[i]+1):cumh[i+1] for i = 1:mi]
+        nranges = [(cumw[j]+1):cumw[j+1] for j = 1:ni]
+        new(sum(hs), sum(ws), M, mranges, nranges, Array{T,1}(max(sum(hs),sum(ws))))
+    end
+end
+BlockMatrix(M::MT) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}} = BlockMatrix{T,MT}(M)
+
+Base.showarray(io::IO, X::BlockMatrix, b::Bool) = display(io, X)
+function Base.display(io::IO, M::BlockMatrix{T,MT}) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}}
+    println(io, "$(size(M,1))x$(size(M,2)) BlockMatrix{$T}:")
+    println(io, "With blocks of types:")
+    Base.print_matrix(io, vcat((hcat(typeof.(Mi)...) for Mi in M.M)...), "[", ", ", "]")
+    println(io, "\nand sizes:")
+    Base.print_matrix(io, vcat((hcat(size.(Mi)...) for Mi in M.M)...), "[", ", ", "]")
+end
+
+import Base.Cartesian: @nexprs
+
+@generated function Base.A_mul_B!(Y::AbstractVector{T}, M::BlockMatrix{T, MT}, B::AbstractVector{T}) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}}
+    ni1 = ni-1
+    quote
+        @assert size(B,2) == size(Y,2) == 1
+        @assert size(M,2) == size(B,1)
+        @assert size(M,1) == size(Y,1)
+
+        @nexprs $mi i -> A_mul_B!(view(Y, M.mranges[i]), M.M[i][1], view(B,M.nranges[1]))
+
+        @nexprs $ni1 j -> begin
+            @nexprs $mi i -> A_mul_B!(view(M.tmp, M.mranges[i]), M.M[i][j+1], view(B,M.nranges[j+1]))
+             Y .+= view(M.tmp, 1:M.m)
+        end
+        return Y
+    end
+end
+
+@generated function Base.At_mul_B!(Y::AbstractVector{T}, M::BlockMatrix{T, MT}, B::AbstractVector{T}) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}}
+    mi1 = mi-1
+    quote
+        @assert size(B,2) == size(Y,2) == 1
+        @assert size(M,1) == size(B,1)
+        @assert size(M,2) == size(Y,1)
+
+        @nexprs $ni j -> At_mul_B!(view(Y, M.nranges[j]), M.M[1][j], view(B,M.mranges[1]))
+
+        @nexprs $mi1 i -> begin
+            @nexprs $ni j -> At_mul_B!(view(M.tmp, M.nranges[j]), M.M[i+1][j], view(B,M.mranges[i+1]))
+             Y .+= view(M.tmp, 1:M.n)
+        end
+        return Y
+    end
+end
+
+
+@generated function Base.Ac_mul_B!(Y::AbstractVector{T}, M::BlockMatrix{T, MT}, B::AbstractVector{T}) where {T, mi, ni, MT <: NTuple{mi,NTuple{ni,AbstractArray{T}}}}
+    mi1 = mi-1
+    quote
+        @assert size(B,2) == size(Y,2) == 1
+        @assert size(M,1) == size(B,1)
+        @assert size(M,2) == size(Y,1)
+
+        @nexprs $ni j -> Ac_mul_B!(view(Y, M.nranges[j]), M.M[1][j], view(B,M.mranges[1]))
+
+        @nexprs $mi1 i -> begin
+            @nexprs $ni j -> Ac_mul_B!(view(M.tmp, M.nranges[j]), M.M[i+1][j], view(B,M.mranges[i+1]))
+             Y .+= view(M.tmp, 1:M.n)
+        end
+        return Y
+    end
+end
+
+Base.size(A::BlockMatrix) = (A.m, A.n)
 
 end # module
